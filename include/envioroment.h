3361730e7ab3b56899bd012920a527f690bcb805
LuisGrigore <luiscristiangrigore11@gmail.com>
2025-09-25 13:59:08 +0200

    Añadido script para autogeneracion de makefile

--- include/envioroment.h
+++ include/envioroment.h
@@ -10,25 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
 #ifndef ENVIOROMENT_H
 # define ENVIOROMENT_H
 
-#include "../include/list.h"
+# include "../include/list.h"
 
 typedef struct s_envioroment_var
 {
-    char *var_name;
-    char *var_value;
-}   t_envioroment_var;
+	char			*var_name;
+	char			*var_value;
+}					t_envioroment_var;
 
-t_envioroment_var *init_envioroment_var();
-t_gen_list *get_environment_var_list_from_str_array(char **str_array);
-char **get_str_array_from_envioroment_var_list(t_gen_list   *envioroment);
-char *get_var_value_from_name(t_gen_list *environment_vars, char *name);
-void destroy_envioroment_var(void *envioroment_var);
-void change_env_value(t_gen_list *envioroment, char *new_value, char *name);
-void remove_envioroment_var_from_name(t_gen_list *envioroment, char *name);
+t_envioroment_var	*init_envioroment_var(void);
+t_gen_list			*get_environment_var_list_from_str_array(char **str_array);
+char				**get_str_array_from_envioroment_var_list(t_gen_list *envioroment);
+char				*get_var_value_from_name(t_gen_list *environment_vars,
+						char *name);
+void				destroy_envioroment_var(void *envioroment_var);
+void				change_env_value(t_gen_list *envioroment, char *new_value,
+						char *name);
+void				remove_envioroment_var_from_name(t_gen_list *envioroment,
+						char *name);
 
-# endif
+#endif
--- include/list.h
+++ include/list.h
@@ -13,24 +13,25 @@
 #ifndef LIST_H
 # define LIST_H
 
-#include <stdlib.h>
+# include <stdlib.h>
 
 typedef struct s_node
 {
-    void *value;
-    struct s_node *next;
-} t_node;
+	void			*value;
+	struct s_node	*next;
+}					t_node;
 
 typedef struct s_gen_list
 {
-    t_node *head;
-    size_t size;
-} t_gen_list;
+	t_node			*head;
+	size_t			size;
+}					t_gen_list;
 
+t_gen_list			*init_list(void);
+void				insert_end(t_gen_list *list, void *value);
+void				destroy_gen_list(t_gen_list *list,
+						void(value_destroyer)(void *));
+void				*unlink_element_from_list(t_gen_list *list,
+						void *element_to_return);
 
-t_gen_list *init_list();
-void insert_end(t_gen_list* list, void* value);
-void destroy_gen_list(t_gen_list* list, void (value_destroyer)(void*));
-void *unlink_element_from_list(t_gen_list *list, void *element_to_return);
-
-# endif
+#endif
