46e041b9f1227423954acc5848df67540c7986c8
LuisGrigore <luiscristiangrigore11@gmail.com>
2025-09-30 02:59:02 +0200

    documentation update

--- docs/html/files_dup.js
+++ docs/html/files_dup.js
@@ -3,6 +3,5 @@ var files_dup =
     [ "include", "dir_d44c64559bbebec7f509842c48db8b23.html", "dir_d44c64559bbebec7f509842c48db8b23" ],
     [ "libft", "dir_e07bdaeef29e43278a85e45cdb4ebfc4.html", "dir_e07bdaeef29e43278a85e45cdb4ebfc4" ],
     [ "list", "dir_48c61abad44e2f181b0a6a33afb59c17.html", "dir_48c61abad44e2f181b0a6a33afb59c17" ],
-    [ "src", "dir_68267d1309a1af8e8297ef4c3efbcdba.html", "dir_68267d1309a1af8e8297ef4c3efbcdba" ],
-    [ "layout.xml", "de/df1/layout_8xml.html", null ]
+    [ "src", "dir_68267d1309a1af8e8297ef4c3efbcdba.html", "dir_68267d1309a1af8e8297ef4c3efbcdba" ]
 ];
--- docs/html/search/files_5.js
+++ docs/html/search/files_5.js
@@ -1,7 +1,6 @@
 var searchData=
 [
-  ['layout_2exml_0',['layout.xml',['../de/df1/layout_8xml.html',1,'']]],
-  ['libft_2eh_1',['libft.h',['../d6/d7e/libft_8h.html',1,'']]],
-  ['list_2ec_2',['list.c',['../d7/d40/list_8c.html',1,'']]],
-  ['list_2eh_3',['list.h',['../da/d23/list_8h.html',1,'']]]
+  ['libft_2eh_0',['libft.h',['../d6/d7e/libft_8h.html',1,'']]],
+  ['list_2ec_1',['list.c',['../d7/d40/list_8c.html',1,'']]],
+  ['list_2eh_2',['list.h',['../da/d23/list_8h.html',1,'']]]
 ];
