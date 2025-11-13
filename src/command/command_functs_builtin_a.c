0e6a1989e2d6f6114bbec0f8471a566f05554ce5
LuisGrigore <LuisGrigore@users.noreply.github.com>
2025-11-13 19:58:30 +0100

    formater

--- src/command/command_functs_builtin_a.c
+++ src/command/command_functs_builtin_a.c
@@ -6,7 +6,7 @@
 /*   By: lgrigore <lgrigore@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 06:40:29 by dmaestro          #+#    #+#             */
-/*   Updated: 2025/11/13 19:21:46 by lgrigore         ###   ########.fr       */
+/*   Updated: 2025/11/13 19:57:36 by lgrigore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,7 +132,7 @@ static int	print_export(t_command *command, t_environment *environment)
 	while (serialized_env[i] != NULL)
 	{
 		if (ft_strncmp("declare -x ?", serialized_env[i],
-			ft_strlen("declare-x ?")) == 0)
+			ft_strlen("declare -x ?")) == 0)
 		{
 			i++;
 			continue ;
